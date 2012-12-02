0b959e7e547be57faf57320c5f63acebb0c51efd
Po Ting Tse <kittytse1102@gmail.com>
2012-12-02 11:19:23 +0000

    ADD: calculate avg scores of a city

---
+++ frontend/javascript/calculate.js
@@ -0,0 +1,17 @@
+function calculateAvg()) {
+	var totalScore = 0;
+	var count = 0;
+
+	//score data from json file  var: scores
+	
+	
+	
+
+	if(count == 0){
+		return 0;
+	}
+	
+	return totalScore/count;
+	
+}
+
--- frontend/javascript/script.js
+++ frontend/javascript/script.js
@@ -1,14 +1,3 @@
-var markers = [
-		['Nairobi', -1.283330, 36.816670, 2000],
-		['Mombasa', -4.050000, 39.666670, "iconRed"],
-		['Nakuru', -0.283330, 36.066670, "iconYellow"],
-		['Kisumu', -0.100000, 34.750000, "iconYellow"],
-	    ['Marsabit', 2.333330, 37.983330, "iconGreen"],
-		['Eldoret', 0.516670, 35.283330, "iconGreen"]
-	];
-
-
-
 function initializeMaps() {
     var myOptions = {
         mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -22,59 +11,38 @@ function initializeMaps() {
     var iconYellow = 'images/yellowLarge.png';
     var iconGreen = 'images/greenLarge.png';
 
-
+    //alert(markers.length);
 
     for (i = 0; i < markers.length; i++) {
-        var pos = new google.maps.LatLng(markers[i][1], markers[i][2]);
+    
+        var latitude = markers[i]["latitude"];
+    	var longitude =  markers[i]["longitude"];
+    	var city =  markers[i]["city"];
+        
+        pos = new google.maps.LatLng(latitude, longitude);
         bounds.extend(pos);
-        //var image = "images/grayLarge.png";
 
-        /*
-        if (markers[i][3] == "iconRed") {
-        image =  "images/grayLarge.png";
-        } else if (markers[i][3] == "iconYellow") { 
-        image =  "images/yellowLarge.png";
-        }else{
-        image =  "images/greenLarge.png";
-        }
-            
-            
-        */
-
-        if (markers[i][3] >= 100) {
+        if (markers[i]["numResults"] >= 100) {
             image = "images/greenLarge.png";
-        } else if (markers[i][3] <= 3) {
-            image = "images/redLarge.png";
-        } else {
+        } else if (markers[i]["numResults"] >3) {
             image = "images/yellowLarge.png";
+        } else {
+            image = "images/redLarge.png";
         }
-
 
         marker = new google.maps.Marker({
             position: pos,
             map: map,
-            title: markers[i][0],
+            title: city,
             icon: image
         });
-
-
-
-        /*
-        marker = new google.maps.Marker({
-        map: map,
-        position: pos,
-        title: markers[i][0],
-        icon: image
-        });
-
-        */
 
         google.maps.event.addListener(marker, 'click', (function (marker, i) {
 
             return function () {
-                infowindow.setContent(markers[i][0]);
+                infowindow.setContent(city);
                 infowindow.open(map, marker);
-                energy.city_clicked(markers[i][0]);
+                energy.city_clicked(city);
             }
         })(marker, i));
     }
